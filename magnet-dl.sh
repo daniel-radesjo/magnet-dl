60ffe725520515315ada2f831569584004283e66
Daniel Rådesjö <daniel.radesjo@gmail.com>
2022-07-13 19:26:02 +0200

    Init

--- magnet-dl.sh
+++ magnet-dl.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if ! command -v wget &> /dev/null; then
   echo "Wget cannot be found"
   exit 1
@@ -9,6 +8,6 @@ for app in axiom ief; do
   CURRENT=$(wget --spider https://magnetforensics.com/dl/${app} 2>&1 | egrep -o "^Adress: .*" | cut -d" " -f2 | rev | cut -d"/" -f1 | rev)
 
   if [ ! -f ${CURRENT} ]; then
-	wget -q -O "${CURRENT}" https://magnetforensics.com/dl/${app}
+    wget -q -O "${CURRENT}" https://magnetforensics.com/dl/${app}
   fi
 done
